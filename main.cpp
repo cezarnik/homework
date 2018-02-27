5c9796b62e23944abe402701a8111611bb7d017e
cezarnik <cezarnik2@yandex.ru>
2018-02-28 01:46:25 +0300

    try output version

--- main.cpp
+++ main.cpp
@@ -4,5 +4,10 @@
 
 int main(int argc, char ** argv){
 	std::cout<<"Hello, World!";
+	#ifdef TRAVIS_BUILD_NUMBER
+		std::cout<<TRAVIS_BUILD_NUMBER<<std::endl;
+	#else
+		std::cout<<"Not defined";
+	#endif
 	return 0;
 }
