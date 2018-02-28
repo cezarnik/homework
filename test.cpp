ab18ee5af7e166431e4ba732e87b408aaf18b58f
cezarnik <cezarnik2@yandex.ru>
2018-02-28 05:51:25 +0300

    added full version and travis

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@
 BOOST_AUTO_TEST_SUITE(test_helloworld)
 
 BOOST_AUTO_TEST_CASE(test_valid_version){
-//	BOOST_CHECK(version().back()-'0' > 0);
+	BOOST_CHECK(version().back()-'0' > 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
