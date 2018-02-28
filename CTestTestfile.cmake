a0401ebaa938aeddd97fe633709d13df4e47954d
cezarnik <cezarnik2@yandex.ru>
2018-02-28 06:36:31 +0300

    qqq

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nick/Documents/OTUS/HW1
-# Build directory: /home/nick/Documents/OTUS/HW1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_helloworld "test_helloworld")
